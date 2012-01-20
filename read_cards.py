d64d73b7c324c4af3792f1247ecb4ec5652adfa8
Dan Padilha <dan@ripenetwork.com>
2012-01-20 20:05:19 +1100

    Fixed line reading.

--- read_cards.py
+++ read_cards.py
@@ -2,8 +2,8 @@ import serial
 rfid = serial.Serial('/dev/ttyUSB0', 115200) # Possibly need a timeout as well
 while 1:
     line = rfid.readline()
-    if line.startswith("UID: "):
-        print line[5:]
+    if line[1:5] == 'UID:':
+        print line[6:]
     # Do some processing on line
     # For example, open the database and look for the UID in the line
     # Add to a counter
