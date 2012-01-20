fb01a1dcad857f9b54bb00c9ed309a545ad6c9c9
Dan Padilha <dan@ripenetwork.com>
2012-01-20 19:47:03 +1100

    Changing README to markdown, only print out UIDs.

--- read_cards.py
+++ read_cards.py
@@ -2,7 +2,8 @@ import serial
 rfid = serial.Serial('/dev/ttyUSB0', 115200) # Possibly need a timeout as well
 while 1:
     line = rfid.readline()
-    print line
+    if line.startswith("UID: "):
+        print line[5:]
     # Do some processing on line
     # For example, open the database and look for the UID in the line
     # Add to a counter
